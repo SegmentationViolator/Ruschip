499a01e500263f36db7be356b01c8b7ec9183f99
Segmentation Violator <segmentationviolator@proton.me>
2026-09-09 00:55:18 +0530

    build: use Windows subsystem for release builds

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
